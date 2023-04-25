f47dfb5fa798217809422a5067883b403cd11236
Sagir <s2200767@edu.bc.fi>
2023-04-25 19:14:41 +0300

    päivitys

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const {host, port} = require("./config.json")
+const { getMovieByCategory, getMovie } = require('./tietovarasto.js')
 const path = require("path")
 
 const express = require("express");
@@ -11,6 +12,31 @@ app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     res.render("home", {});
+});
+
+app.get("/videot", (req, res) => {
+    res.render("videos", {});
+});
+
+
+app.get('/api/videot', async (req, res)=>{
+    try {
+        let page = req.query.page;
+        let category = req.query.category;
+
+        if (typeof page === 'undefined') page = 0;
+
+        if (typeof category === 'undefined') {
+            res.json(await getMovie(Number(page) * 30));
+        }
+        else {
+            res.json(await getMovieByCategory(category, Number(page) * 30));
+        }
+
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+
 })
 
 app.listen(port, host, ()=>{
---
+++ tietovarasto.js
@@ -0,0 +1,61 @@
+let dbconfig = require("./dbconfig.json")
+let mysql = require("mysql")
+
+function getMovieByCategory(category, page) {
+    return new Promise((resolve, reject)=> {
+        let con = mysql.createConnection(dbconfig);
+        con.connect();
+
+        con.query(`select f.film_id, 
+        f.title, 
+        c.name as category,
+        description,
+        f.replacement_cost as price,
+        release_year as year,
+        language_id
+        from film as f, film_category as fc 
+        left outer join category as c on c.category_id = fc.category_id
+        where fc.film_id=f.film_id and upper(c.name)=upper(?)
+        order by f.title ASC
+        limit 30
+        offset ?;`, [category, page], (err, rows, cols) => {
+            if (err) {
+                reject(err.message)
+            }
+
+            con.end();
+            resolve(rows);
+        })
+    
+    })
+}
+
+function getMovie(page) {
+    return new Promise((resolve, reject)=> {
+        let con = mysql.createConnection(dbconfig);
+        con.connect();
+
+        con.query(`select f.film_id, 
+        f.title, 
+        c.name as category,
+        description,
+        f.replacement_cost as price,
+        release_year as year,
+        language_id
+        from film as f, film_category as fc 
+        left outer join category as c on c.category_id = fc.category_id
+        where fc.film_id=f.film_id
+        order by f.title ASC
+        limit 30
+        offset ?;`, [page], (err, rows, cols) => {
+            if (err) {
+                reject(err.message)
+            }
+
+            con.end();
+            resolve(rows);
+        })
+    
+    })
+}
+module.exports = {getMovieByCategory, getMovie}
