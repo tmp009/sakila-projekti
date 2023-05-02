fe3bc24b0c1eeb765fc0446a6b0ea6b87d561be3
Sagir <s2200767@edu.bc.fi>
2023-05-02 16:55:28 +0300

    sivunvaihto nappia lisätty

---
+++ public/js/search.js
@@ -0,0 +1,21 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let search = document.getElementById('search-bar').querySelector('input');
+    let url = new URLSearchParams(window.location.search);
+    const param = url.get('term');
+    if (param) search.value = param;
+});
+
+document.getElementById('search-bar').querySelector('input').addEventListener('keypress', (e)=>{
+    if (e.key !== 'Enter') return;
+    const search = document.getElementById('search-bar').querySelector('input').value;
+    let url = new URLSearchParams(window.location.search);
+    url.delete('page');
+
+    if (search==='') url.delete('term');
+    else {
+        url.set('term', search);
+    }
+    window.location.search=url;
+});
--- public/js/video.js
+++ public/js/video.js
@@ -3,6 +3,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let selected = document.querySelector('select');
     let url = new URLSearchParams(window.location.search);
+    
     const param = url.get('category');
     if (param) selected.value = param;
 });
@@ -18,3 +19,16 @@ document.querySelector('select').addEventListener('change', (e)=>{
     }
     window.location.search=url;
 });
+
+document.querySelector('.page-nav').addEventListener('click', (e)=>{
+    let url = new URLSearchParams(window.location.search);
+    const pageDir = Number(e.target.value);
+    const page = Number(url.get('page'))
+    
+    if (!pageDir) return;
+    
+    if (page) url.set('page', page + pageDir);
+    else {url.set('page', 1 + pageDir) }
+
+    window.location.search=url;
+})
