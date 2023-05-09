d0aac02f22e584d5388771c75a2c3261f53026de
Sagir <s2200767@edu.bc.fi>
2023-05-09 15:12:19 +0300

    palvelin päivitys

--- public/js/search.js
+++ public/js/search.js
@@ -1,24 +1,70 @@
 'use strict';
 
+const dropdown = document.querySelector('.dropdown-content');
+
 document.addEventListener('DOMContentLoaded', ()=>{
-    let search = document.getElementById('search-bar').querySelector('input');
+    let search = document.getElementById('search-bar').querySelector('input[type="text"]');
     let url = new URLSearchParams(window.location.search);
-    const param = url.get('term');
-    if (param) search.value = param;
+    const searchOpt = dropdown.querySelector('input[data-type="1"]');
+    const term = url.get('term');
+    const actor = url.get('actor');
+
+    if (Number(actor)) {
+        searchOpt.setAttribute('checked', '');
+        search.placeholder = 'Hae näyttelijät hakusanalla';
+    }
+
+    if (term) search.value = term;
+    
+    showClearButton(search.value);
 });
 
-document.getElementById('search-bar').querySelector('input').addEventListener('keypress', (e)=>{
-    if (e.key !== 'Enter') return;
+document.getElementById('search-bar').querySelector('input[type="text"]').addEventListener('keypress', (e)=>{
+    const search = document.getElementById('search-bar').querySelector('input[type="text"]').value;
 
-    const search = document.getElementById('search-bar').querySelector('input').value;
+    if (e.key !== 'Enter') return;
     if (search.length != 0 && search.length < 3) return;
-    
+
     let url = new URLSearchParams(window.location.search);
     url.delete('page');
+    url.delete('category');
 
-    if (search==='') url.delete('term');
+    if (dropdown.querySelector('input[data-type="1"]').checked) url.set('actor',  1);
+    else { url.delete('actor') } 
+
+    if (search==='') {
+        url.delete('term');
+        url.delete('actor');
+    }
     else {
         url.set('term', search);
     }
-    window.location.search=url;
+    window.location = '/videot?' + url;
 });
+
+
+document.getElementById('search-bar').querySelector('input[type="text"]').addEventListener('input', (e)=>showClearButton(e.target.value))
+
+document.getElementById('clear-btn').addEventListener('click', (e)=>{
+    document.getElementById('search-bar').querySelector('input[type="text"]').value='';
+    showClearButton('');
+})
+
+dropdown.querySelector('input[data-type="0"]').addEventListener('input', ()=>{
+    const search = document.getElementById('search-bar').querySelector('input[type="text"]')
+    search.placeholder = 'Hae videot hakusanalla';
+});
+
+dropdown.querySelector('input[data-type="1"]').addEventListener('input', ()=>{
+    const search = document.getElementById('search-bar').querySelector('input[type="text"]')
+    search.placeholder = 'Hae näyttelijät hakusanalla';
+});
+
+function showClearButton(search) {
+    const clear = document.getElementById('clear-btn');
+
+    if (search.length) clear.removeAttribute('hidden');
+    else{
+        clear.setAttribute('hidden', true);
+    }
+}
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const {host, port} = require("./config.json")
 const { getMovieByCategory, getMovie, getMovieById, 
-        searchMovie, getMovieCount, getMovieCountSearch } = require('./tietovarasto.js')
+        searchMovie, getMovieCount, getMovieCountSearch, 
+        actorNameSearch, actorNameSearchCount, getMovieByActorId,
+        actorMovieCount, getActorName } = require('./tietovarasto.js');
+
 const path = require("path")
 
 const express = require("express");
@@ -19,9 +22,11 @@ app.get("/videot", async (req, res) => {
     try {
         let page = Number(req.query.page);
         let videos;
+        let actors = [];
         let count;
         const category = req.query.category;
-        const term = req.query.term
+        const term = req.query.term;
+        const actor = Number(req.query.actor);
         
         if (!page) page = 0;
         else { page = page - 1 }
@@ -29,8 +34,14 @@ app.get("/videot", async (req, res) => {
         const offset = page*10;
 
         if (typeof term !== 'undefined') {
-            videos = await searchMovie(offset, term, category);
-            count = await getMovieCountSearch(term, category);
+            if (actor){
+                actors = await actorNameSearch(term, page);
+                count = await actorNameSearchCount(term);
+            } else {
+                videos = await searchMovie(offset, term, category);
+                count = await getMovieCountSearch(term, category);
+            }
+
         } else if (typeof category !== 'undefined') {
             videos = await getMovieByCategory(category, offset);
             count = await getMovieCount(category);
@@ -39,9 +50,41 @@ app.get("/videot", async (req, res) => {
             count = await getMovieCount(category);
         }
 
-        res.render("videos", {videos:videos,
+        res.render("videos", {
+                videos:videos, 
+                actors:actors,
+                isActor:actor,
                 count:count,
                 page:page});
+
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+app.get("/videot/nayttelija/:id", async (req, res) => {
+    try {
+        let page = Number(req.query.page);
+        let videos;
+        let count;
+        const category = req.query.category;
+        const id = Number(req.params.id);
+        
+        if (!page) page = 0;
+        else { page = page - 1 }
+        
+        const offset = page*10;
+
+        videos = await getMovieByActorId(id, offset, category);
+        count = await actorMovieCount(id, category);
+
+        res.render("videos", {
+                videos:videos, 
+                isActor:false,
+                name:await getActorName(id),
+                count:count,
+                page:page
+            });
 
     } catch (error) {
         res.status(400).send(error.message);
@@ -54,7 +97,7 @@ app.get('/videot/:id', async (req,res)=>{
 
         const video = await getMovieById(+id)
 
-        if (!video.length) res.status(404).send('Ei löytyy');
+        if (!video.length) res.status(404).render('notFound');
         else{
             res.render('info', {video:video[0]})
         }
@@ -63,6 +106,10 @@ app.get('/videot/:id', async (req,res)=>{
     } catch (error) {
         res.status(400).send(error.message);
     }
+})
+
+app.get('*', (req,res)=>{
+    res.status(404).render('notFound')
 })
 
 app.listen(port, host, ()=>{
